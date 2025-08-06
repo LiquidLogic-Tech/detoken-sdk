897733b00b7a4d73c126cf1e1cbd3104fd6cc341
Jarek Lin <s410673074@gmail.com>
2025-08-06 12:12:51 +0800

    feat: example

--- sdk/src/lib/constant.ts
+++ sdk/src/lib/constant.ts
@@ -1,7 +1,7 @@
 import { normalizeSuiAddress } from "@mysten/sui/utils";
 
 export const UPGRADE_CAP_OBJECT_ID =
-  "0x1d8e3a69719bc0ec9a9cd701c8d5ad02766fed14c97a787a2aaa5144124d48de";
+  "0xf20e845fc1686a91a198c16467316ed518e3297646a75d3829a452427ef935e1";
 export const ADMIN_CAP_OBJECT_ID =
-  "0x1b4b153d8e66898ba0952aeef0bf0748265ba1601df7f59b8724d5499385f338";
+  "0xe584ae63a40a89758983feb424f5977b316aa068867ecb45ff0ee3281ddc5e86";
 export const DUMMY_ADDRESS = normalizeSuiAddress("0x00");
